e4e5a5c595c156575cc6ddc5e549c353d44c098b
777aman <111171358+777aman@users.noreply.github.com>
2022-09-17 11:57:03 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,25 +1,33 @@
 #include "main.h"                                                                          
+                                                                                        
+/**                                                                                                                                                                                   
+ * largest_number - returns the largest of 3 numbers                                                                                                                                  
+* @a: first integer                                                                                                                                                               
+* @b: second integer                                                                                                                                                                
+* @c: third integer                                                                                                                                                                 
+* Return: largest number                                                                                                                                                            
+*/                                                                                        
                                                                                            
-                                                                                           
-                                                                                           
-/**                                                                                        
-                                                                                           
- * largest_number - returns the largest of 3 numbers                                       
-                                                                                           
- * @a: first integer                                                                       
-                                                                                           
- * @b: second integer                                                                      
-                                                                                           
- * @c: third integer                                                                       
-                                                                                           
- * Return: largest number                                                                  
-                                                                                           
- */                                                                                        
-                                                                                           
-int largest_number(int a, int b, int c)                                                    
-                                                                                           
+int largest_number(int a, int b, int c)                                                                                                                                               
 {                                                                                          
-                                                                                           
-    int largest;                                                                           
-"2-largest_number.c" 38L, 369C                                1,8           Top            
+int largest;                                                                           
+
+if (a > b && a > c)
+{
+      largest = a;
+}
+else if (b > c && b > a)
+{
+  largest = b;
+}
+  else if (c > b)
+  { 
+    largest = c;
+  }
+  else 
+  { 
+    largest = b;
+  }
+  return (largest);
+}
                                  
