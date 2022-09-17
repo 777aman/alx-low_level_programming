8f535bfa28ad7592ef665ca05a7f5aaa5deb7908
777aman <111171358+777aman@users.noreply.github.com>
2022-09-17 23:39:04 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
 * main - causes an infinite loop
@@ -17,7 +17,8 @@ i = 0;
 *putchar(i);
 *}
 */
-printf("Infinite loop avoided! \\o/\n");
+
+  printf("Infinite loop avoided! \\o/\n");
 
   return (0);
 }
