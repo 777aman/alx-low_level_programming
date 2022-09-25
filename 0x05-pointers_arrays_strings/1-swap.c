2a23e80e76e9b64aed13a8ce946a134e5213c6e1
777aman <111171358+777aman@users.noreply.github.com>
2022-09-25 22:14:21 +0300

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,26 +1,24 @@
-#include "main.h"                                                  
-
-                                                                   
-
-/**                                                                
-
- * swap_int - Swaps the values of two integers.                    
-
- * @a: The first integer to be swapped.                            
-
- * @b: The second integer to be swapped.                           
-
- */                                                                
-
-void swap_int(int *a, int *b)                                      
-
-{                                                                  
-
-        int tmp = *a;                                              
-
-        *a = *b;                                                   
-
-        *b = tmp;                                                  
-
+                                                                                                                                     
+/**                                                                                                                                  
+ * _strncat - Concatenates two strings using at most                                                                                 
+ *            an inputted number of bytes from src.                                                                                  
+ * @dest: The string to be appended upon.                                                                                            
+ * @src: The string to be appended to dest.                                                                                          
+ * @n: The number of bytes from src to be appended to dest.                                                                          
+ *                                                                                                                                   
+ * Return: A pointer to the resulting string dest.                                                                                   
+ */                                                                                                                                  
+char *_strncat(char *dest, char *src, int n)                                                                                         
+{                                                                                                                                    
+        int index = 0, dest_len = 0;                                                                                                 
+                                                                                                                                     
+        while (dest[index++])                                                                                                        
+                dest_len++;                                                                                                          
+                                                                                                                                     
+        for (index = 0; src[index] && index < n; index++)                                                                            
+                dest[dest_len++] = src[index];                                                                                       
+                                                                                                                                     
+        return (dest);                                                                                                               
 }
+
 
