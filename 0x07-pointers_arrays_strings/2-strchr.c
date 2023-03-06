9ed0c664bcb8e31155e30c2218e3afb471dd1f81
777aman <amanueltarekegn90@gmail.com>
2023-03-06 11:41:03 -0800

    3. strspn

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,17 @@
-1-memcpy.c
 #include "main.h"
 /**
- *_memcpy - a function that copies memory area
- *@dest: memory where is stored
- *@src: memory where is copied
- *@n: number of bytes
- *
- *Return: copied memory with n byted changed
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_strchr(char *s, char c)
 {
-        int r = 0;
-        int i = n;
-        for (; r < i; r++)
+        int i = 0;
+        for (; s[i] >= '\0'; i++)
         {
-                dest[r] = src[r];
-                n--;
+                if (s[i] == c)
+                        return (&s[i]);
         }
-        return (dest);
+        return (0);
 }
